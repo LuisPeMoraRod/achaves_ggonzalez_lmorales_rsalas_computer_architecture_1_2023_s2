f27ea90e1a9805aae9856da6b3db8a89189dcf76
ChipsAchoy <49498818+ChipsAchoy@users.noreply.github.com>
2023-11-19 17:22:46 -0600

    Código de arduino traducido a C

--- main/main.c
+++ main/main.c
@@ -15,15 +15,30 @@
 #include "services/gatt/ble_svc_gatt.h"
 #include "sdkconfig.h"
 
-#define PROXIMITY_DIG_INPUT GPIO_NUM_22 //pin 22 for digital input (infrared proximity sensor)
 
 #define LIGHT_INPUT_PIN ADC1_CHANNEL_0 // pin 0 used as analog input (photoresistor)
+#define TEMP_INPUT_PIN ADC1_CHANNEL_1 // pin 1 used as analog input (termistor)
+
 #define LED_0 GPIO_NUM_8 // pin 8 for first LED
 #define LED_1 GPIO_NUM_1 // pin 1 for second LED
-#define LIGHT_THRESHOLD 2000 //threshold to turn on LEDs
+
+#define MOTOR_1_A GPIO_NUM_18 // pin 18 for first pin motor driver A
+#define MOTOR_1_B GPIO_NUM_19 // pin 19 for second pin motor driver A
+#define MOTOR_2_A GPIO_NUM_20 // pin 20 for first pin motor driver B
+#define MOTOR_2_B GPIO_NUM_21 // pin 21 for second pin motor driver B
+
+#define FAN_OUT GPIO_NUM_23 //pin 23 for fan output
+
+#define PROXIMITY_DIG_INPUT GPIO_NUM_22 //pin 22 for digital input (infrared proximity sensor)
+#define LIGHT_THRESHOLD 1500 //threshold to turn on LEDs
+#define TEMP_THRESHOLD 30.0 //threshold to turn fan out
+
+
 
 char *TAG = "BLE-Server";
 uint8_t ble_addr_type;
+uint8_t lockFront = 0;
+
 void ble_app_advertise(void);
 
 // Write data to ESP32 defined as server
@@ -46,13 +61,46 @@ static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_g
     data[data_len] = '\0';
 
     //control motors by reading data sent from client
-    if (strcmp(data, (char *)"1010\0")==0)
+    if ((strcmp(data, (char *)"1010\0")==0) && (lockFront == 0))
     {
-       printf("Move forward\n");
+        printf("Move forward\n");
+        gpio_set_level(MOTOR_1_A, 1);
+        gpio_set_level(MOTOR_2_A, 0);
+        gpio_set_level(MOTOR_1_B, 1);
+        gpio_set_level(MOTOR_2_B, 0);
+
     }
     else if (strcmp(data, (char *)"0101\0")==0)
     {
         printf("Move backwards\n");
+        gpio_set_level(MOTOR_1_A, 0);
+        gpio_set_level(MOTOR_2_A, 1);
+        gpio_set_level(MOTOR_1_B, 0);
+        gpio_set_level(MOTOR_2_B, 1);
+    }
+    else if ((strcmp(data, (char *)"1000\0")==0)  && (lockFront == 0))
+    {
+        printf("Move right\n");
+        gpio_set_level(MOTOR_1_A, 1);
+        gpio_set_level(MOTOR_2_A, 0);
+        gpio_set_level(MOTOR_1_B, 0);
+        gpio_set_level(MOTOR_2_B, 0);
+    }
+    else if ((strcmp(data, (char *)"0010\0")==0)  && (lockFront == 0))
+    {
+        printf("Move left\n");
+        gpio_set_level(MOTOR_1_A, 0);
+        gpio_set_level(MOTOR_2_A, 0);
+        gpio_set_level(MOTOR_1_B, 1);
+        gpio_set_level(MOTOR_2_B, 0);
+    }
+    else if (strcmp(data, (char *)"0000\0")==0)
+    {
+        printf("Stop\n");
+        gpio_set_level(MOTOR_1_A, 0);
+        gpio_set_level(MOTOR_2_A, 0);
+        gpio_set_level(MOTOR_1_B, 0);
+        gpio_set_level(MOTOR_2_B, 0);
     }
     else{
         printf("Unrecognized command: %s\n", data);
@@ -154,11 +202,14 @@ void leds_control(){
     // 11dB attenuation (ADC_ATTEN_DB_11) gives full-scale voltage 0 - 3.9V
     // 4053 ~ 3.86V
     lightVoltage = adc1_get_raw(LIGHT_INPUT_PIN); 
-    printf("%d\n", lightVoltage);
+    printf("Current light: %d\n", lightVoltage);
 
     if (lightVoltage <= LIGHT_THRESHOLD){
         gpio_set_level(LED_0, 1); //turn LED 0 on
-        gpio_set_level(LED_1, 1); //turn LED 0 on
+        gpio_set_level(LED_1, 1); //turn LED 1 on
+    }else{
+        gpio_set_level(LED_0, 0); //turn LED 0 off
+        gpio_set_level(LED_1, 0); //turn LED 1 off
     }
 
 }
@@ -167,7 +218,52 @@ void leds_control(){
 void proximity_control(){
     if (gpio_get_level(PROXIMITY_DIG_INPUT)){
         printf("Obstacle approaching...");
+        lockFront = 1;
+    } else{
+        lockFront = 0;
     }
+}
+
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void temperature_control(){
+    /*
+    Takes the adc1 channel 1 
+    */
+    const float Rc = 10000.0; //valor de la resistencia
+    const float Vcc = 5.0;
+    //const int SensorPIN = A1;
+
+    float A = 1.11492089e-3;
+    float B = 2.372075385e-4;
+    float C = 6.954079529e-8;
+
+    float K = 2.5; //factor de disipacion en mW/C
+
+    float tempVoltage = (float)adc1_get_raw(TEMP_INPUT_PIN); 
+    float tempCorrected = mapFloat(tempVoltage, 0.0, 5000.0, 0.0, 1023.0);
+    float V =  tempCorrected / 1024.0 * Vcc;
+
+    float R = (Rc * V ) / (Vcc - V);
+  
+
+    float logR  = log(R);
+    float R_th = 1.0 / (A + B * logR + C * logR * logR * logR );
+
+    float kelvin = R_th - V*V/(K * R)*1000.0;
+    float celsius = kelvin - 277.15;  
+
+    printf("Current temperature: %d\n", celsius);
+    
+    if (celsius >= TEMP_THRESHOLD){
+        gpio_set_level(FAN_OUT, 1); //turn LED 0 on
+    }else{
+        gpio_set_level(FAN_OUT, 0); //turn LED 0 on
+    }
+
 }
 
 void app_main()
@@ -176,11 +272,19 @@ void app_main()
     //set digital GPIO modes
     gpio_set_direction(LED_0, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED_1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(FAN_OUT, GPIO_MODE_OUTPUT);
     gpio_set_direction(PROXIMITY_DIG_INPUT, GPIO_MODE_INPUT);
+    gpio_set_direction(LIGHT_INPUT_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(TEMP_INPUT_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(MOTOR_1_A, GPIO_MODE_OUTPUT);
+    gpio_set_direction(MOTOR_1_B, GPIO_MODE_OUTPUT);
+    gpio_set_direction(MOTOR_2_A, GPIO_MODE_OUTPUT);
+    gpio_set_direction(MOTOR_2_B, GPIO_MODE_OUTPUT);
 
     //set analog input (light sensor)
     adc1_config_width(ADC_WIDTH_BIT_12); //Configure ADC1 capture width: 12 bit decimal value from 0 to 4095
     adc1_config_channel_atten(LIGHT_INPUT_PIN, ADC_ATTEN_DB_11); //Configure the ADC1 channel (ADC1_CHANNEL_0), and setting attenuation (ADC_ATTEN_DB_11)
+    adc1_config_channel_atten(TEMP_INPUT_PIN, ADC_ATTEN_DB_11); //Configure the ADC1 channel (ADC1_CHANNEL_1), and setting attenuation (ADC_ATTEN_DB_11)
 
     //set BLE connection
     nvs_flash_init();                          // Initialize NVS flash using
@@ -199,7 +303,8 @@ void app_main()
         
         leds_control();
         proximity_control();
+        temperature_control();
 
-        vTaskDelay(100 / portTICK_PERIOD_MS); //100 ms delay
+        vTaskDelay(500 / portTICK_PERIOD_MS); //100 ms delay
     }
 }
